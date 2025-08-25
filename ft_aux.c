6524c98d1d3ad57a17eec56621334c1c337dc9f1
Delpi <daviddelpinomorales33@gmail.com>
2025-08-25 17:34:32 +0200

    AllThePrintf

---
+++ ft_aux.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_aux.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddel-pin <ddel-pin@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 18:34:19 by ddel-pin          #+#    #+#             */
+/*   Updated: 2024/10/18 18:43:27 by ddel-pin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	handle_character_and_string(const char *format, int *i, va_list args,
+		int *count)
+{
+	char	*str;
+
+	if (format[*i] == 'c')
+	{
+		ft_putchar_fd(va_arg(args, int), 1);
+		(*count)++;
+	}
+	else if (format[*i] == 's')
+	{
+		str = va_arg(args, char *);
+		if (!str)
+		{
+			ft_putstr_fd("(null)", 1);
+			(*count) += 6;
+		}
+		else
+		{
+			ft_putstr_fd(str, 1);
+			(*count) += ft_strlen(str);
+		}
+	}
+}
+
+void	handle_numbers_and_pointers(const char *format, int *i, va_list args,
+		int *count)
+{
+	char	*str;
+
+	if (format[*i] == 'p')
+		ft_putptr(va_arg(args, void *), count);
+	else if (format[*i] == 'd' || format[*i] == 'i')
+	{
+		str = ft_itoa(va_arg(args, int));
+		ft_putstr_fd(str, 1);
+		(*count) += ft_strlen(str);
+		free(str);
+	}
+	else if (format[*i] == 'u')
+		ft_putnbr_unsigned(va_arg(args, unsigned int), count);
+}
+
+void	aux(const char *format, int *i, va_list args, int *count)
+{
+	(*i)++;
+	handle_character_and_string(format, i, args, count);
+	handle_numbers_and_pointers(format, i, args, count);
+	if (format[*i] == 'x')
+		ft_putnbr_hex(va_arg(args, unsigned int), 0, count);
+	else if (format[*i] == 'X')
+		ft_putnbr_hex(va_arg(args, unsigned int), 1, count);
+	else if (format[*i] == '%')
+	{
+		ft_putchar_fd('%', 1);
+		(*count)++;
+	}
+}
